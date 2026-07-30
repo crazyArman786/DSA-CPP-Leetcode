fda92aea27ef47e4e64fe2347b7037999ff160da
crazyArman786 <armanbharadwaj7@gmail.com>
2026-07-30 23:45:05 +0530

    Add files via upload

---
+++ Q68.cpp
@@ -0,0 +1,24 @@
+// You are given an array of characters letters that is sorted in non-decreasing order, and a 
+// character target. There are at least two different characters in letters.
+
+// Return the smallest character in letters that is lexicographically greater than target. If such 
+// a character does not exist, return the first character in letters.
+
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
+        int start = 0, end=n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(letters[mid]<=target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return letters[start % letters.size()];
+    }
+};
