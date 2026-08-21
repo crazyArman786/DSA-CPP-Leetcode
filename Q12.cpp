456b29d2ea12daaa5ce0961f94026cf28c88b95e
crazyArman786 <armanbharadwaj7@gmail.com>
2026-08-21 21:18:30 +0530

    Add files via upload

---
+++ Q12.cpp
@@ -0,0 +1,22 @@
+// Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+// Return the kth positive integer that is missing from this array.
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int start = 0, end = arr.size()-1, mid, ans=arr.size();
+        while(start<=end){
+            mid = start +(end-start)/2;
+            if(arr[mid]-mid-1 >= k){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans+k;
+    }
+}; 
---
+++ Q13.cpp
@@ -0,0 +1,40 @@
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start=0, end=n-1, mid, ans=-1;
+        while(start<=end){
+            mid = start +(end-start)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                break;
+            }
+            else if(nums[mid]>=nums[0]){
+                if(nums[start]<=target && nums[mid]>= target){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<= target && nums[end]>=target){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return ans;
+    }
+};
